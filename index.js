875e00a3934f31b0d0822055e27e26f754d64b8f
Siwakornzz <Siwakornzz@hotmail.com>
2021-09-07 04:04:12 +0700

    update error,fix message send not work yet

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
+const User = require('./models/User')
+require('./utils/db')
 
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   return res.render('index')
+})
+
+app.get('/register', (req, res) => {
+  return res.render('register')
+})
+
+app.post('/register', async(req, res) => {
+  const user = new User(req.body)
+  await user.save()
+  return res.render('register',{message:"Register Success"})
 })
 app.listen(3000, () => {
   console.log('Server Running at port 3000')
--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Siwakornzz:Aunda132@cluster0.8fj52.mongodb.net/lolipopz');
+mongoose.connect('mongodb+srv://Siwakornzz:Aunda132@cluster0.8fj52.mongodb.net/lolipopz',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 mongoose.connection.once('open', ()=>{
     console.log("Database Connected :) ");
