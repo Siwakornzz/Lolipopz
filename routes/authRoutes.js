b936ecc3a3cd04c6dc81dba2d1869ecc359d4da4
Siwakornzz <Siwakornzz@hotmail.com>
2021-09-10 23:59:19 +0700

    add middleware login check request api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { addUser } = require('../modules/users/service/userService');
-const { registerSchema } = require('../modules/users/validations/AuthValidations');
-const { joiErrorFormatter , mongooseErrorFormatter } = require('../utils/validationFormatter');
+const { addUser } = require("../modules/users/service/userService");
+const {
+  registerSchema,
+} = require("../modules/users/validations/AuthValidations");
+const {
+  joiErrorFormatter,
+  mongooseErrorFormatter,
+} = require("../utils/validationFormatter");
+
+const m1 = (req, res, next) => {
+  req.user = "Guest";
+  next();
+};
+
+const m2 = (req,res,next)=>{
+  console.log(req.url)
+  next();
+};
+
 /**
  * Show page for user registration
  */
- router.get('/register', (req, res) => {
-  return res.render('register', { message: {},formData:{},errors:{}})
-})
+router.get("/register", (req, res) => {
+  return res.render("register", { message: {}, formData: {}, errors: {} });
+});
 /**
  * Handles user registration
  */
@@ -16,55 +32,64 @@ router.post("/register", async (req, res) => {
   try {
     //check validationResult
     const validationsResult = registerSchema.validate(req.body, {
-      abortEarly: false
-    })
-    if (validationsResult.error){
-      return res.render('register',{
-        message:{
-          type:'danger',
-          body:'Validation Errors'
+      abortEarly: false,
+    });
+    if (validationsResult.error) {
+      return res.render("register", {
+        message: {
+          type: "danger",
+          body: "Validation Errors",
         },
-        errors:joiErrorFormatter(validationsResult.error),
-        formData : req.body
-        })
+        errors: joiErrorFormatter(validationsResult.error),
+        formData: req.body,
+      });
     }
 
     const user = await addUser(req.body);
-    return res.render('register', { message:{
-      type: 'success',
-      body: 'Registration Success'
-    }, 
-      errors: {}, 
-      formData: req.body  
+    return res.render("register", {
+      message: {
+        type: "success",
+        body: "Registration Success",
+      },
+      errors: {},
+      formData: req.body,
     });
   } catch (e) {
     console.error(e);
-    return res.status(400).render('register',{
-      message:{
-        type:'danger',
-        body:'Validation Errors'
+    return res.status(400).render("register", {
+      message: {
+        type: "danger",
+        body: "Validation Errors",
       },
-        errors:mongooseErrorFormatter(e),
-        formData : req.body
-      })
+      errors: mongooseErrorFormatter(e),
+      formData: req.body,
+    });
   }
 });
 
 /**
  * Show page for user login
  */
- router.get('/login', (req, res) => {
-  return res.render('login', { message: {},formData:{},errors:{}})
-})
+router.get("/login", (req, res) => {
+  return res.render("login", { message: {}, formData: {}, errors: {} });
+});
 
 /**
  * login a user
  */
- router.post('/login', (req, res) => {
-  return res.render('login', { message: {
-    type:'success',
-    body: 'Login Successfully'
-  },formData:{},errors:{}})
-})
+router.post("/login", m1,m2, (req, res) => {
+  console.log(req.user);
 
+  return res.render("login", {
+    message: {
+      type: "success",
+      body: "Login Successfully",
+    },
+    formData: {},
+    errors: {},
+  });
+});
+
+// login middleware
+const logMid = (req, res, next) => {};
 module.exports = router;
