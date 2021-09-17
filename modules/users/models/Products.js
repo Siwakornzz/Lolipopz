11a1bdddc317f026da930cb4e9e3e6549c45db90
klotza5678 <klotza5678@gmail.com>
2021-09-17 08:20:58 +0700

    Edit model

--- modules/users/models/Products.js
+++ modules/users/models/Products.js
@@ -1,30 +1,34 @@
 const mongoose = require('mongoose')
 
 const productsSchema = mongoose.Schema({
-    name: {
-        type: String
-    },
-    img: {
-        type: String
-    },
-    version: {
-        type: String,
-        default : "1.0.0"
-    },
-    price: {
-        type: Number,
-        default: 0
-    },
-    description:{
-        type: String,
-        
-    }
-}, {
-    timestamps: true
+            name: {
+                type: String
+            },
+            img: {
+                type: String
+            },
+            version: {
+                type: String,
+                default: "1.0.0"
+            },
+            price: {
+                type: Number,
+                default: 0
+            },
+            description: {
+                type: String,
 
-})
+            },
+            type: [{
+                userid: {
+                    type: ObjectId
+                }
+            }, {
+                timestamps: true
+
+            })
 
 
-const Products = mongoose.model('products', productsSchema)
+            const Products = mongoose.model('products', productsSchema)
 
-module.exports = Products
+            module.exports = Products
